5ae110bf73edf75050c00e8c8b3287fea0bb6df2
Bara Suchanova <suchanova.barbora@gmail.com>
2020-04-11 21:56:45 +0200

    Setup amplify auth, storage, change icons, titles, colors

---
+++ src/app/user-profile-tab/user-profile.page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-profile',
+  templateUrl: 'user-profile.page.html',
+  styleUrls: ['user-profile.page.scss'],
+})
+export class UserProfilePage {
+
+  constructor() { }
+}
